5e55e1b99f2151a9de6630d048fd1b7ddf74022f
LesPantsFancy <mjnrock@umich.edu>
2022-11-20 20:13:22 -0500

    init

--- client/src/routes/Default.jsx
+++ client/src/routes/Default.jsx
@@ -4,6 +4,7 @@ import { TagString } from "./../lib/dms/tags/TagString";
 import { TagInt8 } from "./../lib/dms/tags/TagInt8";
 
 //TODO: The FormControl tag and the TagString do not presently coordinate the fact that they're both tags and that TagString isn't just a JS String.
+//IDEA: This was a quick test, and may not prove to work as imagined; consider revaluating this approach and see if it needs adjustment or abandoning.
 
 const formControl = new FormControl(
 	FormControl.EnumFormControlType.INPUT,
