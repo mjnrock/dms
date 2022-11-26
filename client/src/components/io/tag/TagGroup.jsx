212412e90515ae45134eafb2fef2ed6444145d96
LesPantsFancy <mjnrock@umich.edu>
2022-11-26 08:30:13 -0500

    init

--- client/src/components/io/tag/TagGroup.jsx
+++ client/src/components/io/tag/TagGroup.jsx
@@ -1,3 +1,8 @@
+//TODO: Imlpement a drag and drop feature for child tags
+//* Implement List and ListItem wrapper components so isolate the dnd logic
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
+
 import { Tag } from "./Tag";
 import { Factory } from "./packaage";
 
@@ -11,10 +16,14 @@ export function TagGroup({ tag, isEditing = false } = {}) {
 			) }
 			view={ (t, v) => {
 				return (
-					//FIXME: @isEditing should be controlled, as should the component-specific props (i.e. either an event emission or prop passing)
-					t.state.map(t => Factory(t, { key: t.id, isEditing: true }))
+					<DndProvider backend={ HTML5Backend }>
+						{
+							//FIXME: @isEditing should be controlled, as should the component-specific props (i.e. either an event emission or prop passing)
+							t.state.map(t => Factory(t, { key: t.id, isEditing: true }))
+						}
+					</DndProvider>
 				);
-			}}
+			} }
 		/>
 	);
 };
