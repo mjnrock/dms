562c6513ef0acd178a192aeee75df4d030aa186f
LesPantsFancy <mjnrock@umich.edu>
2022-12-18 15:26:08 -0500

    init

--- client/src/routes/Default.jsx
+++ client/src/routes/Default.jsx
@@ -64,7 +64,7 @@ let baseTag = new TagNamespace("root", [
 
 // console.table(Serializer.ToHierarchy(baseTag), [ "id", "pid", "alias", "dtype", "value", "path" ]);
 // console.log(Serializer.ToHierarchyObject(baseTag));
-// console.table(Serializer.ToHierarchyRecord(baseTag));
+console.table(Serializer.ToHierarchyRecord(baseTag));
 // console.log(Serializer.ToHierarchyRecordObject(baseTag));
 
 export function Default() {
