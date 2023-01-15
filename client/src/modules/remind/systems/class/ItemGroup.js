116ce22585a2cf2804c443a2228041b199b5925f
LesPantsFancy <mjnrock@umich.edu>
2023-01-15 13:56:24 -0500

    init

--- client/src/modules/remind/systems/class/ItemGroup.js
+++ client/src/modules/remind/systems/class/ItemGroup.js
@@ -23,6 +23,32 @@ export const ItemGroup = {
 
 		return parent;
 	},
+	replaceChild(parent, oldChild, newChild) {
+		const index = parent.state.children.indexOf(oldChild);
+
+		if(index !== -1) {
+			parent.state.children[index] = newChild;
+			newChild.state.parent = parent;
+			oldChild.state.parent = null;
+		}
+
+		dispatch(parent, "update", parent.state.children);
+
+		return parent;
+	},
+	swapPosition(parent, child1, child2) {
+		const index1 = parent.state.children.indexOf(child1);
+		const index2 = parent.state.children.indexOf(child2);
+
+		if(index1 !== -1 && index2 !== -1) {
+			parent.state.children[index1] = child2;
+			parent.state.children[index2] = child1;
+		}
+
+		dispatch(parent, "update", parent.state.children);
+
+		return parent;
+	},
 };
 
 export default ItemGroup;
